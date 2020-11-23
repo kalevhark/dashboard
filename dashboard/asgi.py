08c6c9410056197ad0ea1c605f84a02b0451857c
kalevhark <Kalev.Hark@mail.ee>
2020-11-23 16:35:28 +0200

    redis

--- dashboard/asgi.py
+++ dashboard/asgi.py
@@ -9,7 +9,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+import django
+
 from django.core.asgi import get_asgi_application
+
+django.setup()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
 
--- dashboard/settings.py
+++ dashboard/settings.py
@@ -139,7 +139,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('192.168.1.143', 6379)],
+            "hosts": [('127.0.0.1', 6379)],
         },
     },
 }
